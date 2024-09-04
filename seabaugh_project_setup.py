d6e7926055f52bf21f1e467f498add9ab1a1ca3b
Clayton Seabaugh <claytonseabaugh@gmail.com>
2024-09-04 17:19:45 -0500

    Comments outlining project plan (Iteration 1)

--- seabaugh_project_setup.py
+++ seabaugh_project_setup.py
@@ -1,1 +1,20 @@
-print("starting project 2")
+'''Data Project No 2!!
+
+This project demonstrates proficiency in loops, project folder creation using pathlib, and importing modules.
+
+This module provides functions for creating a series of project folders
+'''
+
+#Import modules from python libraries
+import pathlib
+
+#Import local modules
+import seabaugh_utils
+
+#Define functions for folder creation
+
+#Add Join Paths with pathlib
+
+#Define Main Function
+
+#Conditional Script Execution
