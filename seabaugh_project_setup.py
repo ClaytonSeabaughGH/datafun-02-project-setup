4f77947b8a0c35542128d70d03d494ba31ebede5
Clayton Seabaugh <claytonseabaugh@gmail.com>
2024-09-04 17:28:36 -0500

    Defined Functions that will be used (Iteration 2)
    My plan next is to add the global variables.

--- seabaugh_project_setup.py
+++ seabaugh_project_setup.py
@@ -11,9 +11,27 @@ import pathlib
 #Import local modules
 import seabaugh_utils
 
+#Declare Global Variables
+
+
+
+
 #Define functions for folder creation
 
-#Add Join Paths with pathlib
+
+#Function 1: Generates folders for a given range (years)
+def create_folders_for_range(start_year, end_year: int) -> None:
+
+
+#Function 2: Creates folders from a list of names 
+def create_folders_from_list(folder_list: int) -> None:
+
+#Function 3: Creates prefixed folders by combinging a list of names with a prefix
+def create_prefixed_folders(folder_list, prefix: str) -> None:
+
+#Function 4: A While Loop that creates folders periodically
+def create_folders_periodically(duration):
+
 
 #Define Main Function
 
